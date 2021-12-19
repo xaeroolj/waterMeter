d15fe6fdd805ccff382e250b6e6fce8876ddfe54
Roman Trekhlebov <romantrekhlebov@192-168-1-75.local>
2021-12-19 18:50:20 +0300

    temp

--- Sources/waterFlowMeter/main.swift
+++ Sources/waterFlowMeter/main.swift
@@ -2,22 +2,32 @@ import Foundation
 import SwiftyGPIO
 
 print("Start")
+
+func saveLog() {
+    let fileManager = FileManager.default
+    let filePath = fileManager.currentDirectoryPath.appending("/logFile.txt")
+    let url = URL(fileURLWithPath: filePath)
+    do {
+        let fileHandle = try FileHandle(forWritingTo: url)
+        fileHandle.seekToEndOfFile()
+        // convert your string to data or load it from another resource
+        let str = "\(Date()) +1\n"
+        let textData = Data(str.utf8)
+        // append your text to your text file
+        fileHandle.write(textData)
+        // close it when done
+        fileHandle.closeFile()
+    } catch {
+        
+    }
+    
+}
+
 let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPiPlusZero)
 var gp = gpios[.P26]!
 
-
-gp.onRaising{
-    gpio in
-    print("Transition to 1, current value:" + String(gpio.value))
-}
 gp.onFalling{
     gpio in
-    print("Transition to 0, current value:" + String(gpio.value))
+    saveLog()
 }
-gp.onChange{
-    gpio in
-//    gpio.clearListeners()
-    print("The value changed, current value:" + String(gpio.value))
-}  
-
 RunLoop.main.run()
